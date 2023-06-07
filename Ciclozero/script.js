869ac541b1afd61905dc8c05772859922f7dda88
netxor17 <61363012+netxor17@users.noreply.github.com>
2023-06-07 18:53:49 +0200

    añadido archivo para enseñar

--- Ciclozero/script.js
+++ Ciclozero/script.js
@@ -1,8 +1,25 @@
 'use strict'
 
+const top = document.querySelector('.elementor-element-9a1c492');
 
-const nav = document.querySelectorAll('.elementor-section');
+document.addEventListener('DOMContentLoaded', function() {
+    console.log(top)
+});
 
-console.log(nav);
-console.log('hola')
-console.clear();
+const stickyNav = function(entries){
+    const [entry] = entries;
+    console.log(entry);
+    if(!entry.isIntersecting)
+    top.classList.add('sticky');
+    else top.classList.remove('sticky');
+  };
+  
+  const options = {
+    root: null, //null es para el viewport del navegador
+    threshold: 0, //cuando no se vea nada
+    rootMargin:  '-90px'
+  };
+  
+  const headerObserver = new IntersectionObserver(stickyNav,options);
+  
+  headerObserver.observe(top);
